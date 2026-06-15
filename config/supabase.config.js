b7eddc8da01a36271755f1bb7e7fea5f6b62859c
artesamia.cn <artesamia.cn@gmail.com>
2026-06-15 18:18:12 -0400

    Se agrega integración con MercadoPago

--- config/supabase.config.js
+++ config/supabase.config.js
@@ -1,8 +1,6 @@
 const SUPABASE_CONFIG = {
-  //API_URL: '___VALOR_API_URL___',
-  //API_KEY: '___VALOR_API_KEY___'
-  API_URL: 'https://zbqrlyvjgfeycczixgqc.supabase.co/',
-  API_KEY: 'sb_publishable_sE5jMRoQ-SNjuSMWJSY63g_OEGn5h8e'
+  API_URL: '___VALOR_API_URL___',
+  API_KEY: '___VALOR_API_KEY___'
 };
 
 if (typeof window !== 'undefined') window.SUPABASE_CONFIG = SUPABASE_CONFIG;
